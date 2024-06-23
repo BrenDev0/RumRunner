b2da6bfb5212b3d6793e2fa355039eb5087037c9
Brendan Lahey <lahey1991@gmail.com>
2024-06-23 11:23:56 -0500

    filter file

--- components/list_files.py
+++
@@ -1,7 +0,0 @@
-import os 
-
-def list_files(string):
-    files = [ f for f in os.listdir("C:/Program Files") if string in f]
-    print(files)
-
-
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from PySide6.QtWidgets import QApplication   
 import sys 
-
+from components.file_filter import exe_files
 
 app = QApplication(sys.argv)
+
+exe_files('firefox')
+
+
+
+
 
 
 
